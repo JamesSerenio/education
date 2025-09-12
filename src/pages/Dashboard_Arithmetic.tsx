361ed2fe70ee84a9d116806f16dc5421b2c7dffc
JamesSerenio <sereniojames363@gmail.com>
2025-09-12 08:58:31 +0800

    added code for the 2 dashboard

--- src/pages/Dashboard_Arithmetic.tsx
+++ src/pages/Dashboard_Arithmetic.tsx
@@ -1,17 +1,56 @@
-import React from "react";
-import { IonPage, IonContent, IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonMenu,
+  IonMenuToggle,
+  IonPage,
+  IonRouterOutlet,
+  IonSplitPane,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { homeOutline, logOutOutline, rocketOutline } from "ionicons/icons";
 
 const Dashboard_Arithmetic: React.FC = () => {
+  const path = [
+    { name: "Home", url: "/education/home", icon: homeOutline },
+    { name: "About", url: "/education/about", icon: rocketOutline },
+  ];
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Dashboard Arithmetic</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <p>Welcome to the Arithmetic Sequence Dashboard!</p>
-      </IonContent>
+      <IonSplitPane contentId="main">
+        <IonMenu contentId="main">
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            {path.map((item, index) => (
+              <IonMenuToggle key={index}>
+                <IonItem routerLink={item.url} routerDirection="forward">
+                  <IonIcon icon={item.icon} slot="start" />
+                  {item.name}
+                </IonItem>
+              </IonMenuToggle>
+            ))}
+
+            {/* Logout Button */}
+            <IonButton routerLink="/education/home" routerDirection="back" expand="full">
+              <IonIcon icon={logOutOutline} slot="start" />
+              Logout
+            </IonButton>
+          </IonContent>
+        </IonMenu>
+
+        <IonRouterOutlet id="main">
+          {/* Dito mo ilalagay ang mga nested routes o content ng dashboard */}
+        </IonRouterOutlet>
+      </IonSplitPane>
     </IonPage>
   );
 };

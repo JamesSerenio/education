bb001c457a89e4beec2548a3d22c0798a3c764af
JamesSerenio <sereniojames363@gmail.com>
2025-09-15 08:34:53 +0800

    Updates

--- assets/swipe-back-legacy-CFMxDoCg.js
+++ assets/swipe-back-legacy-CFMxDoCg.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-xzOjy2SG.js","./index3-legacy-BMuwGcF2.js"],function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CygA1lx1.js","./index3-legacy-DgHG_P8J.js"],function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

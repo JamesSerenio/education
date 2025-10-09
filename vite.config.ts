a92068f4cbebcd497ef148e143eb74c41059e027
JamesSerenio <sereniojames363@gmail.com>
2025-10-10 01:02:19 +0800

    fix: vite base path and vercel rewrite

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), legacy()],
-  base: "/",            // ✅ must be root path only
+  base: process.env.VITE_BASE_PATH || "/education",            // ✅ must be root path only
   test: {
     globals: true,
     environment: "jsdom",

115714715f7a3545074eec061d623531cb06fd0f
JamesSerenio <sereniojames363@gmail.com>
2025-10-10 11:38:44 +0800

    fix vercel route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route, HashRouter } from "react-router-dom";
 
+/* Pages */
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 
+/* Dashboards */
 import Dashboard_Arithmetic from "./Dashboard/Arithmetic_Sequence/Dashboard_Arithmetic";
 import Dashboard_Motion from "./Dashboard/Uniform_Motion_in_Physics/Dashboard_Motion";
-import Arithmetic_Practice from "./Dashboard/Arithmetic_Sequence/Arithmetic_Practice";
 
+/* Arithmetic Sequence pages */
+import Arithmetic_Practice from "./Dashboard/Arithmetic_Sequence/Arithmetic_Practice";
 import ArithmeticHome from "./Dashboard/Arithmetic_Sequence/Arithmetic_Home";
 import ArithmeticModule from "./Dashboard/Arithmetic_Sequence/Arithmetic_Module";
 import ArithmeticLeaderboard from "./Dashboard/Arithmetic_Sequence/Arithmetic_Leaderboard";
 import ArithmeticRadar from "./Dashboard/Arithmetic_Sequence/Arithmetic_Radar";
 import ArithmeticQuiz from "./Dashboard/Arithmetic_Sequence/arithmetic_quiz";
 
+/* Motion pages */
 import MotionQuiz from "./Dashboard/Uniform_Motion_in_Physics/motion_quiz";
 import MotionPractice from "./Dashboard/Uniform_Motion_in_Physics/Motion_Practice";
-// ✅ Admin Dashboard
+
+/* Admin */
 import AdminDashboard from "./admin/admin_dashboard";
 
-/* Core CSS required for Ionic components to work properly */
+/* Ionic Core CSS */
 import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
@@ -29,7 +34,7 @@ import "@ionic/react/css/normalize.css";
 import "@ionic/react/css/structure.css";
 import "@ionic/react/css/typography.css";
 
-/* Optional CSS utils that can be commented out */
+/* Optional CSS utils */
 import "@ionic/react/css/padding.css";
 import "@ionic/react/css/float-elements.css";
 import "@ionic/react/css/text-alignment.css";
@@ -37,99 +42,54 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
-/* Ionic Dark Mode */
+/* Dark mode */
 import "@ionic/react/css/palettes/dark.system.css";
 
-/* Tailwind + Global CSS */
+/* Tailwind + Global */
 import "./global.css";
-
-/* Theme variables */
 import "./theme/variables.css";
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        {/* 🔹 Auth Pages */}
-        <Route exact path="/education/login" component={Login} />
-        <Route exact path="/education/register" component={Register} />
+    {/* ✅ Use HashRouter to fix Vercel white screen issue */}
+    <HashRouter>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          {/* 🔹 Auth Pages */}
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
 
-        {/* 🔹 Home */}
-        <Route exact path="/education/home" component={Home} />
+          {/* 🔹 Home */}
+          <Route exact path="/home" component={Home} />
 
-        {/* 🔹 Dashboards */}
-        <Route
-          exact
-          path="/education/dashboard_arithmetic"
-          component={Dashboard_Arithmetic}
-        />
-        <Route
-          exact
-          path="/education/dashboard_motion"
-          component={Dashboard_Motion}
-        />
+          {/* 🔹 Dashboards */}
+          <Route exact path="/dashboard_arithmetic" component={Dashboard_Arithmetic} />
+          <Route exact path="/dashboard_motion" component={Dashboard_Motion} />
 
-        {/* 🔹 Arithmetic Practice */}
-        <Route
-          exact
-          path="/education/arithmetic_practice"
-          component={Arithmetic_Practice}
-        />
+          {/* 🔹 Arithmetic Practice & Subpages */}
+          <Route exact path="/arithmetic_practice" component={Arithmetic_Practice} />
+          <Route exact path="/arithmetic_home" component={ArithmeticHome} />
+          <Route exact path="/arithmetic_module" component={ArithmeticModule} />
+          <Route exact path="/arithmetic_leaderboard" component={ArithmeticLeaderboard} />
+          <Route exact path="/arithmetic_radar" component={ArithmeticRadar} />
+          <Route exact path="/arithmetic_quiz" component={ArithmeticQuiz} />
 
-        {/* 🔹 Arithmetic Sub Pages */}
-        <Route
-          exact
-          path="/education/arithmetic_home"
-          component={ArithmeticHome}
-        />
-        <Route
-          exact
-          path="/education/arithmetic_module"
-          component={ArithmeticModule}
-        />
-        <Route
-          exact
-          path="/education/arithmetic_leaderboard"
-          component={ArithmeticLeaderboard}
-        />
-        <Route
-          exact
-          path="/education/arithmetic_radar"
-          component={ArithmeticRadar}
-        />
-        <Route
-          exact
-          path="/education/arithmetic_quiz"
-          component={ArithmeticQuiz}
-        />
+          {/* 🔹 Motion Practice & Quiz */}
+          <Route exact path="/motion_practice" component={MotionPractice} />
+          <Route exact path="/motion_quiz" component={MotionQuiz} />
 
-        <Route
-          exact
-          path="/education/motion_practice"
-          component={MotionPractice}
-        />
+          {/* 🔹 Admin Dashboard */}
+          <Route exact path="/admin_dashboard" component={AdminDashboard} />
 
-         <Route
-          exact
-          path="/education/motion_quiz"
-          component={MotionQuiz}
-        />
-
-        {/* 🔹 Admin Dashboard */}
-        <Route
-          exact
-          path="/education/admin/admin_dashboard"
-          component={AdminDashboard}
-        />
-
-        {/* 🔹 Default Redirect */}
-        <Route exact path="/education/">
-          <Redirect to="/education/login" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+          {/* 🔹 Default Redirect */}
+          <Route exact path="/">
+            <Redirect to="/login" />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
+    </HashRouter>
   </IonApp>
 );
 

25e6741b0715bcba8671c796295851ae9aa6f260
JamesSerenio <sereniojames363@gmail.com>
2025-10-10 12:43:16 +0800

    Updates

--- assets/md.transition-legacy-CdbND-zP.js
+++ assets/md.transition-legacy-CdbND-zP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DknUAwjs.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DNpfKO14.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

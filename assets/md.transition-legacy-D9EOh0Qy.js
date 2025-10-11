db73a97e5ec8be356a8c749488aca4eae0be10f8
JamesSerenio <sereniojames363@gmail.com>
2025-10-11 17:23:07 +0800

    Updates

--- assets/md.transition-legacy-D9EOh0Qy.js
+++ assets/md.transition-legacy-D9EOh0Qy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-r-lyfScU.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-0OjHiK5u.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

98b7feae94e6ecf54d8799ad6fbf08e31b38631b
JamesSerenio <sereniojames363@gmail.com>
2025-10-12 22:50:24 +0800

    Updates

--- assets/ios.transition-legacy-DYSXG5V6.js
+++ assets/ios.transition-legacy-DYSXG5V6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CxIBKBdN.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DHMpDVcf.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

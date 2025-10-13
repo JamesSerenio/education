d167c262f61f5257109e1478454989fd599354b4
JamesSerenio <sereniojames363@gmail.com>
2025-10-13 14:20:10 +0800

    Updates

--- assets/ios.transition-legacy-9IaXkNSF.js
+++ assets/ios.transition-legacy-9IaXkNSF.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BvxlCMq_.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-B8ftT8kk.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

834fbe3621d020702238057b67585aa89c935a54
JamesSerenio <sereniojames363@gmail.com>
2025-10-13 15:01:39 +0800

    Updates

--- assets/ios.transition-legacy-BKak1jZQ.js
+++ assets/ios.transition-legacy-BKak1jZQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BIbxX74C.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-2FhGtKxE.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-DFLy5zaX.js
+++ assets/md.transition-legacy-DFLy5zaX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BIbxX74C.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-2FhGtKxE.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

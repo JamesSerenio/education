44c29a8b6d9011d508d55f5d304864832ec5c38f
JamesSerenio <sereniojames363@gmail.com>
2025-10-13 17:06:11 +0800

    Updates

--- assets/md.transition-legacy-CBWHonVP.js
+++ assets/md.transition-legacy-CBWHonVP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-YiOONWZB.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-B66v5Sw-.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

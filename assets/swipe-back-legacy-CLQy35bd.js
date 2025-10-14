53d8b05e3f67b04d7e9218bd6aac1d33ee0d9c06
JamesSerenio <sereniojames363@gmail.com>
2025-10-14 18:59:08 +0800

    Updates

--- assets/swipe-back-legacy-CLQy35bd.js
+++ assets/swipe-back-legacy-CLQy35bd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CotT_3G1.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-DmqGWHcB.js"],function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

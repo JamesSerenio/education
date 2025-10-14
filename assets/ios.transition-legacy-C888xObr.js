e40641e467d924d9c6d75bad8ed639939e0ee9b3
JamesSerenio <sereniojames363@gmail.com>
2025-10-14 21:13:23 +0800

    Updates

--- assets/ios.transition-legacy-C888xObr.js
+++ assets/ios.transition-legacy-C888xObr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DmqGWHcB.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BBK6P6Fy.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
